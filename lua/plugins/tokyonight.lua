c98a64c666dfdb71b5f69e69890775c8ec5bef07
Philip Bizimis <bizimisphilip@gmail.com>
2023-05-30 22:25:44 +0200

    Add transparent background

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -1,9 +1,12 @@
-  return {
-    "folke/tokyonight.nvim",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      -- load the colorscheme here
-      vim.cmd([[colorscheme tokyonight-night]])
-    end,
-  }
+return {
+	"folke/tokyonight.nvim",
+	lazy = false, -- make sure we load this during startup if it is your main colorscheme
+	priority = 1000, -- make sure to load this before all the other start plugins
+	opts = {
+		transparent = true,
+	},
+	config = function(_, opts)
+		require("tokyonight").setup(opts)
+		vim.cmd([[colorscheme tokyonight-night]])
+	end,
+}
